5ce2aa550a47531f5c420873e0b919b7fade6156
MarkGelman <47790935+MarkGelman@users.noreply.github.com>
2019-03-13 20:43:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace charim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+            Console.WriteLine("{0}{1}{2}{3}{4}{5}{6}", (char)Convert.ToInt32(s[0]), (char)Convert.ToInt32(s[1]), (char)Convert.ToInt32(s[2]), (char)Convert.ToInt32(s[3]), (char)Convert.ToInt32(s[4]), (char)Convert.ToInt32(s[5]), (char)Convert.ToInt32(s[6]));
+        }
+    }
+   
+}
